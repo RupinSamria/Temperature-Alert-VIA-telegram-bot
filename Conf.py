10f939f6f6a08a73b534a40cb244c30c53b35c3e
Rupin Samria <43100245+RupinSamria@users.noreply.github.com>
2020-01-06 17:32:25 +0530

    Add files via upload

---
+++ Conf.py
@@ -0,0 +1,5 @@
+bolt_api_key = ""                   # Bolt Cloud API Key
+device_id = ""                      # Bolt Device ID
+telegram_chat_id = "@XXXX"          # channel ID of Telegram channel.
+telegram_bot_id = "botXXXX"         # bot ID of Telegram Bot.
+threshold = 250                     # Threshold beyond which the alert should be sent
